8a3f9c60caa33a9c1e26f6883911a35f11656e06
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-08-28 10:06:47 +0530

    correct forget Password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  newPassword: {
+    type: String
+},
   phoneNumber: {
     type: String,
     required: true
